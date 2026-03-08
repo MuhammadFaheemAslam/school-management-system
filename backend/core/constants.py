0a06d4e137535326d4d796cd6a903c753b497f81
Faheem Aslam <faheem17171@gmail.com>
2026-03-08 11:18:46 -0500

    Add the core features those will share accrose the project

---
+++ backend/core/constants.py
@@ -0,0 +1,12 @@
+USER_ROLES = (
+    ("admin", "Admin"),
+    ("teacher", "Teacher"),
+    ("student", "Student"),
+    ("parent", "Parent"),
+)
+
+ATTENDANCE_STATUS = (
+    ("present", "Present"),
+    ("absent", "Absent"),
+    ("late", "Late"),
+)
