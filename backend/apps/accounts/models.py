71f7037ffd01f09dc5f6062fe89a40fad7cff0ac
Faheem Aslam <faheem17171@gmail.com>
2026-03-08 11:41:16 -0500

    Clean Up the code

--- backend/apps/accounts/models.py
+++ backend/apps/accounts/models.py
@@ -1,15 +1,10 @@
 from django.db import models
+from core.constants import USER_ROLES
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    ROLE_CHOICES = (
-        ("ADMIN", "Admin"),
-        ("TEACHER", "Teacher"),
-        ("STUDENT", "Student"),
-        ("PARENT", "Parent"),
-    )
     
-    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    role = models.CharField(max_length=20, choices=USER_ROLES)
     
     def __str__(self):
         return f"{self.username} - {self.role}"
